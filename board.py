09d229b3125bee45712be88afa19bd5c21938fe2
RiWieland <rich.wieland73@gmail.com>
2023-11-22 19:44:39 +0100

    initial commit

---
+++ board.py
@@ -0,0 +1,6 @@
+from cards import card
+from typing import List
+
+class board:
+    def __init__(self, card: List[card]):
+        self.card = card
---
+++ cards.py
@@ -0,0 +1,8 @@
+class card:
+    '''
+    Class for card
+    '''
+    def __init__(self, symbol: str, value: str):
+        self.symbol = symbol
+        self.value = value
+
