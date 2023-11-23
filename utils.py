6a36428d642d4cfa8a842959629daa76d0897cbf
RiWieland <rich.wieland73@gmail.com>
2023-11-23 12:23:16 +0100

    init untils

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+import random
+
 from typing import List
 from cards import card
 
 def print_deck(deck: List[card]):
-    for _card in deck:
-        print(_card)
+    for card_ in deck:
+        print(card_)
 
+def draw_random_card(deck: List[card]) -> card:
+    card_ = random.choice(deck)   
+    return card_
