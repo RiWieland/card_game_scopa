5a79ab20fd12f868a746db5c45684e802a09cde7
RiWieland <rich.wieland73@gmail.com>
2024-01-06 13:20:15 +0100

    update main for fast api

--- main.py
+++ main.py
@@ -12,34 +12,35 @@ from game_logic import get_all_players_cards
 from board import board
 #from export_classes import export_card_class
 
+# API settings
+app = FastAPI()
+
+@app.get("/api/v1/cards_player_smart")
+async def get_player_smart_cards():
+    return player_smart_.cards_hand
+
+@app.get("/api/v1/cards_player_random")
+async def get_player_random_cards():
+    return player_random_.cards_hand
+    
+@app.get("/api/v1/cards_board")
+async def get_open_cards():
+    return board_.cards_open
+
 
 # init dealer and serve cards
 dealer_ = dealer.get_deck(deck=create_deck())
 play_deck, player_cards_1, player_cards_2, cards_open = dealer_.initial_serve()
-board_ = board(cards_open)
+board_ = board(cards_open, cards_offset_=[])
 
 # init players
 player_smart_ = player_smart(player_cards_1)
 player_random_ = player_random(player_cards_2)
 
 # Round one: Both player play card
-
 player_random_.play_card(random_card(player_random_.cards_hand))
 board_.add_card_to_cards_open(random_card(player_smart_.cards_hand))
 
 player_smart_.play_card(random_card(player_smart_.cards_hand))
 board_.add_card_to_cards_open(random_card(player_smart_.cards_hand))
 
-
-
-
-print((player_smart_.cards_hand))
-
-
-
-
-app = FastAPI()
-
-@app.get("/api/v1/cards")
-async def get_players_card():
-    return all_cards(List[export_card_class])
